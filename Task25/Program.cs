92d9b57feab1f0921d4c6c60f23a253658c9a4f9
gunderak <sergey.gund@gmail.com>
2023-02-14 22:37:07 +0300

    add Task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -2,14 +2,17 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.WriteLine("Введите число A: ");
+Console.Write("Введите число A: ");
 int num1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите число B:");
+Console.Write("Введите число B:");
 int num2 = int.Parse(Console.ReadLine());
-int Pow(int number1, int number2)
-{
-    int result = Math.Pow(num1, num2);
-    return result;
-}
 int pow = Pow(num1, num2);
 Console.Write(pow);
+int Pow(int number1, int number2)
+{
+    int result = (int)Math.Pow(num1, num2);// гугл поведал, что Math.Pow возвращает значение типа double,
+    // но при определении типа переменной result как double я получал ошибку:
+    // "Не удается неявно преобразовать тип "double" в "int"" в следующей строке с "return result". В итоге нашёл решение с приведением
+    // к типу int, но всё равно до конца не понял, в чём была ошибка. Буду благодарен за пояснения:)
+    return result;
+}
